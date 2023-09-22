ea52e318e39b024d5007569657df1de586682ad0
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-09-22 01:24:52 -0400

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,5 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
