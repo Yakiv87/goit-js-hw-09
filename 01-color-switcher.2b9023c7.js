b26d26d505cb5eed00f4e259eec7cb33856f1a3d
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-09-26 03:48:39 +0000

    Deploying to gh-pages from @ Yakiv87/goit-js-hw-09@cfeacf1ff9cd0c6bc12300f14c853a3424c96e8a 🚀

---
+++ 01-color-switcher.2b9023c7.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body"),a=null;e.setAttribute("disabled",""),t.addEventListener("click",(function(t){t.target.setAttribute("disabled",!0),e.removeAttribute("disabled"),a=setInterval((function(){r.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(e){e.target.setAttribute("disabled",!0),t.removeAttribute("disabled"),clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.2b9023c7.js.map
